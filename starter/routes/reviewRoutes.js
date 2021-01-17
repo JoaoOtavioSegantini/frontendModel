9c864f167fe5664c0d13d12451fb82eae00d0f8f
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-01-17 10:39:54 -0300

    Refactoring: set mergeParams

--- starter/routes/reviewRoutes.js
+++ starter/routes/reviewRoutes.js
@@ -1,7 +1,7 @@
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
 .route('/')
--- starter/routes/tourRoutes.js
+++ starter/routes/tourRoutes.js
@@ -2,11 +2,21 @@ const express = require('express')
 
 const tourController = require('./../controllers/tourController')
 const authController = require('../controllers/authController')
-const reviewController = require('./../controllers/reviewController')
+const reviewRouter = require('./../routes/reviewRoutes')
 
 const router = express.Router();
 
 //router.param('id', tourController.checkID)
+// POST / tour / 12f3ad21/ reviews
+// GET /tour/545df66/reviews
+//GET/tour/55gfgs/reviews/9022ffdda
+
+/* router
+.route('/:tourId/reviews')
+.post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
+ */
+router.use('/:tourId/reviews', reviewRouter);
+
 router
  .route('/top-5-cheap')
  .get(tourController.aliasTopTours, tourController.getAllTours)
@@ -29,14 +39,6 @@ router
 .get(tourController.getTour)
 .patch(tourController.updateTour)
 .delete(authController.protect, authController.restrictTo('admin','lead-guide',), tourController.deleteTour)
-
-// POST / tour / 12f3ad21/ reviews
-// GET /tour/545df66/reviews
-//GET/tour/55gfgs/reviews/9022ffdda
-
-router
-.route('/:tourId/reviews')
-.post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
 
 
 module.exports = router;
